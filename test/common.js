9c9e9b0dfd0b51e8f2213b1c2df17c62a9ff1ff2
hiseka <kshkonan@gmaill.com>
2014-07-07 20:37:42 +0900

    portfolio
    portfolio

--- test/common.js
+++ test/common.js
@@ -2,22 +2,30 @@ function resized(){
 	var ww=$(window).width();
 	if(1140 >= ww && ww > 1024){
 		$('.Cons').width(1025);
-		$('#mainnav').width(945);
+		$('#header').height(120);
+		$('.mainnav').width(945).height(100+'%');
 	}else if(1024 >= ww && ww > 768){
 		$('.Cons').width(767);
-		$('#mainnav').width(687);
+		$('#header').height(120);
+		$('.mainnav').width(687).height(100+'%');
 	}else if(768 >= ww){
-		$('.Cons').width(ww);
+		$('.Cons').width(100+'%');
 		$('#header').height(250);
-		$('#mainnav').width(ww-80);
-		$('.logospace').width(100+'%').height(50+'%');
-		$('#logo').css({padding:'22.5px 0px',margin:'0 auto'});
+		$('.mainnav').width(100+'%').height(50+'%');
 	}else{
 		$('.Cons').width(1140);
 		$('#header').height(120);
-		$('#mainnav').width(1060);
-		$('.logospace').width(80).height(100+'%');
-		$('#logo').css({padding:'20px 0px'});
+		$('.mainnav').width(1060).height(100+'%');
+	}
+	
+	if(768 >= ww){
+		$('.headerCon > div:eq(1)').addClass('logospaceM').removeClass('logospace');
+		$('h1').addClass('logoM').removeClass('logo');
+		$('.mainnav > ul').addClass('mainnavConM').removeClass('mainnavCon');
+	}else{
+		$('.headerCon > div:eq(1)').addClass('logospace').removeClass('logospaceM');
+		$('h1').addClass('logo').removeClass('logoM');
+		$('.mainnav > ul').addClass('mainnavCon').removeClass('mainnavConM');
 	}
 }
 $(window).resize(function(){
